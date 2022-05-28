d3d1a9086df86057b6e569456d1aa7da3a908890
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-05-28 15:13:14 +0300

    views:created three functions addPhoto,viewPhoto,gallery

--- photoshare/photos/views.py
+++ photoshare/photos/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Category,Photo
 # Create your views here.
 
 def gallery(request):
+    category = request.GET.get('category')
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)
+        
+    
+    
+    
+    
     categories = Category.objects.all()
-    photos = Photo.objects.all()
+    # photos = Photo.objects.all()
     context = { 'categories': categories,'photos': photos}
     return render(request, 'photos/gallery.html',context)
 
@@ -17,10 +27,20 @@ def addPhoto(request):
     if request.method == 'POST':
         data = request.POST
         image = request.FILES.get('image')
-        print('data:', data)
-        print('image:', image)
-    
-    
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+            
+        photo = Photo.objects.create(
+          category=category,
+          description=data['description'],
+          image=image, 
+        )
+        
+        return redirect('gallery')
     context = { 'categories': categories}
     return render(request, 'photos/add.html',context)
 
